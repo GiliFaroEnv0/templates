f447744a138ba24cd1eeb7b453d2554f126d43fc
GiliFaroEnv0 <92937594+GiliFaroEnv0@users.noreply.github.com>
2023-10-04 23:21:04 +0300

    Update main.tf

--- module-registry/main.tf
+++ module-registry/main.tf
@@ -1,9 +1,4 @@
-module "elad" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/elad/bla"
-  bucket_name = "bukbukbuk"
-  version = "1.0.0"
+module "private_gh_module" {
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/private_gh_module/avner"
+  version = "v1.0.0"
 }
-  
-resource null_resource "bla" {
-}
-
