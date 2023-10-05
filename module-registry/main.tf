0a039550879b217e82b23d1c8429ac5ad5e37ba0
GiliFaroEnv0 <92937594+GiliFaroEnv0@users.noreply.github.com>
2023-10-05 09:05:30 +0300

    Update main.tf

--- module-registry/main.tf
+++ module-registry/main.tf
@@ -1,4 +1,4 @@
 module "private_gh_module" {
-  source = "api-13311.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/private_gh_module/avner"
-  version = "v1.0.0"
+  source = "api-pr13311.dev.env0.com/c320beec-2627-491e-99b9-b81ea8bfc26a/private_gh_module/avner"
+  version = "1.0.0"
 }
